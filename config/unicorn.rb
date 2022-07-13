ab220ff6f5a267ce9db476369ef0bdcadea3340c
MathieuDerelle <mathieu@purchease.com>
2022-07-13 10:01:10 +0200

    rubocop -a

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'fileutils'
 preload_app true
 timeout 5
 worker_processes 1
 listen '/tmp/nginx.socket', backlog: 1024
 
-before_fork do |server,worker|
-      FileUtils.touch('/tmp/app-initialized')
+before_fork do |_server, _worker|
+  FileUtils.touch('/tmp/app-initialized')
 end
